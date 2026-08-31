689a38e2d5155f18ea8ede53257d20894d5f9037
dakovtun <kvtndmtrlksvch@gmail.com>
2026-08-31 13:18:26 +0300

    Update SITE_URL to new domain

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_NAME = 'Боулинг СПб'
 export const SITE_DESCRIPTION =
   'Гид по боулинг-центрам Санкт-Петербурга: адреса, цены на дорожки, часы работы и отзывы. Выбирайте клуб и бронируйте дорожку.'
-export const SITE_URL = 'https://bowling-spb.vercel.app'
+export const SITE_URL = 'https://bowling-saint-petersburg.ru'
