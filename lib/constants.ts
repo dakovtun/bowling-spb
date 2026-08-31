6778998f581f478fe9088e3f0841657eda434d03
dakovtun <kvtndmtrlksvch@gmail.com>
2026-08-31 17:54:35 +0300

    Add files via upload

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_NAME = 'Боулинг СПб'
 export const SITE_DESCRIPTION =
   'Гид по боулинг-центрам Санкт-Петербурга: адреса, цены на дорожки, часы работы и отзывы. Выбирайте клуб и бронируйте дорожку.'
-export const SITE_URL = 'https://bowling-saint-petersburg.ru'
+export const SITE_URL = 'https://www.bowling-saint-petersburg.ru'
