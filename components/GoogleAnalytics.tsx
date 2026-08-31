35ca82881d1e810cf1a50129ce706a623edde827
dakovtun <kvtndmtrlksvch@gmail.com>
2026-08-31 15:55:37 +0300

    Add files via upload

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+
+const GA_ID = 'G-3Z6XTHWY8V'
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  )
+}
